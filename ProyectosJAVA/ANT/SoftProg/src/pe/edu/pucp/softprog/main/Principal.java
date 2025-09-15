5a18d9097675096e958cbd6095467e03fafdd4e4
GinoG <a20230500@pucp.edu.pe>
2025-09-15 16:01:13 -0500

    Se han guardado los cambios

---
+++ ProyectosJAVA/ANT/SoftProg/src/pe/edu/pucp/softprog/main/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.pucp.softprog.main;
+
+import com.mysql.cj.admin.ServerController;
+
+/**
+ *
+ * @author Gino
+ */
+public class Principal {
+    public static void main(String[] args){
+        matematicas.Sumatoria objSum = new matematicas.Sumatoria();
+        objSum.imprimir(10, 20);
+        
+        
+    }
+}
